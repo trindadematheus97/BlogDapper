96c8bf968bdda463ea111a03ebebd044b45bb92a
trindadematheus97 <trindadedt3@gmail.com>
2022-10-25 19:43:14 -0300

    Add project files.

---
+++ BlogDapper/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Dapper.Contrib.Extensions;
+
+namespace Blog.Models
+{
+    [Table("[Tag]")]
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+
+        [Write(false)]
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ BlogDapper/Program.cs
@@ -0,0 +1,101 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogDapper
+{
+    public class Program
+    {
+        const string connectionString = "Server=DESKTOP-O1775Q7;Database=Blog;User ID=sa;Password=85882678;Encrypt=false";
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            ReadUsers();
+            //ReadUser();
+            //InsertUser();
+            //UpdateUser();
+            //DeleteUser();
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var users = connection.GetAll<User>();
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"{user.Name}  {user.Id}");
+                }
+            }
+
+
+        }
+
+        public static void ReadUser()
+        {
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+
+            }
+
+        }
+
+        public static void InsertUser()
+        {
+            var user = new User()
+            {
+                Name = "Balta",
+                Bio = "Equipe Balta",
+                Email = "balta@gmail.com",
+                Image = "http://...",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Insert<User>(user);              
+                Console.WriteLine("Usuário cadastrado com sucesso.");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id=5,
+                Name = "Balta.io",
+                Bio = "Equipe Balta",
+                Email = "balta@gmail.com",
+                Image = "http://...",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Usuário atualizado com sucesso.");
+            }
+
+
+        }
+
+        public static void DeleteUser()
+        {
+           
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var user = connection.Get<User>(5);
+                connection.Delete<User>(user);
+                Console.WriteLine("Exclusão realizada com sucesso.");
+            }
+        }
+
+    }   
+}   
